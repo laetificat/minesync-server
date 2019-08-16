c51afe769a9de9e5cdb85b18717bb90a0b107c3c
Kevin Heruer <k.heruer@gmail.com>
2019-08-16 16:11:51 +0200

    Added listing of saves on server

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
+	"path/filepath"
+	"time"
 )
 
 // SyncFolder defines where the savegames will reside
@@ -15,20 +17,29 @@ type syncObject struct {
 	Data []byte
 }
 
-func main() {
-	server()
+type saveList struct {
+	Saves []save
 }
 
-// Set the server and listen for incomming connections
-func server() {
-	ln, err := net.Listen("tcp", ":9999")
-	defer ln.Close()
+type save struct {
+	Name             string
+	LastModifiedDate time.Time
+}
+
+func main() {
+	go startSyncServer()
+	startResourceServer()
+}
+
+// Set the server and listen for incoming requests
+func startResourceServer() {
+	ln, err := net.Listen("tcp", ":9998")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Listening for connections...")
+	fmt.Println("Listening for connections on 9998...")
 	for {
 		c, err := ln.Accept()
 		if err != nil {
@@ -36,12 +47,54 @@ func server() {
 			return
 		}
 
-		go handleServerConnection(c)
+		go handleResourceServerConnection(c)
+	}
+}
+
+// Handles the listing of savegames
+func handleResourceServerConnection(c net.Conn) {
+	files, err := ioutil.ReadDir(SyncFolder)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	saves := make([]save, 0)
+	for _, f := range files {
+		if filepath.Ext(SyncFolder+"/"+f.Name()) == ".zip" {
+			saves = append(saves, save{f.Name(), f.ModTime()})
+		}
+	}
+
+	err = gob.NewEncoder(c).Encode(saveList{saves})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// Set the server and listen for incomming connections
+func startSyncServer() {
+	ln, err := net.Listen("tcp", ":9999")
+	defer ln.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Listening for connections on 9999...")
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		go handleSyncServerConnection(c)
 	}
 }
 
 // Handle the client connection
-func handleServerConnection(c net.Conn) {
+func handleSyncServerConnection(c net.Conn) {
 	defer c.Close()
 
 	decoded := syncObject{}
